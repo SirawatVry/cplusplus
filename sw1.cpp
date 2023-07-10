8dea6fba10309591740d0155a48034f045f3c4f6
SirawatVry <139094315+SirawatVry@users.noreply.github.com>
2023-07-10 10:43:12 +0700

    Add files via upload

---
+++ sw1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{	cout<<"student code : 55-060216-9999-9" << endl; 
+	cout<<"student name : Somchai Cheingpongpan"<< endl; 
+	cout<<"    Division : Information Technology " << endl; 
+	cout<<"        Year : 1 "<< endl;
+	cout<<"        Room : RB " << endl;
+	cout<<"         Sex : male " << endl;
+	cout<<"         age : 19 " <<endl;
+	system("pause");
+	return(0);
+}
